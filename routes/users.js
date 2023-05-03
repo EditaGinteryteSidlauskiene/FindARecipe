7bac1c3d6b4cbb85403d431a4a66b0f3fd8e8b68
EditaGinteryteSidlauskiene <122540484+EditaGinteryteSidlauskiene@users.noreply.github.com>
2023-05-03 10:07:54 +0300

    Update users.js
    Comments added

--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,5 @@
+//This file consists of routes for users
+
 const express = require('express');
 const router = express.Router();
 const passport = require('passport');
@@ -5,13 +7,19 @@ const catchAsync = require('../utils/catchAsync');
 const users = require('../controllers/users');
 
 router.route('/register')
+    //Displaying register form
     .get(users.renderRegister)
+    //Saving a new user
     .post(catchAsync(users.register));
 
 router.route('/login')
+    //Displaying a login form
     .get(users.renderLogin)
+    //Logging in the user
     .post(passport.authenticate('local', { failureFlash: true, failureRedirect: '/login', keepSessionInfo: true }), users.login);
 
+//Logging out the user
 router.get('/logout', users.logout);
 
+//Exporting the router
 module.exports = router;
