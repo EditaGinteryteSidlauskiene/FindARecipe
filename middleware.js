c918ffbb74cf3544a08f5beffda966f6c280f227
EditaGinteryteSidlauskiene <122540484+EditaGinteryteSidlauskiene@users.noreply.github.com>
2023-05-03 11:52:38 +0300

    Update middleware.js
    Comments added

--- middleware.js
+++ middleware.js
@@ -1,7 +1,10 @@
+//This file consists of middlewares
+
 const { campgroundSchema } = require('./schemas.js');
 const Recipe = require('./models/recipe');
 const Review = require('./models/reviews');
 
+//A middleware to check if the user is logged in
 module.exports.isLoggedIn = (req, res, next) => {
     if (!req.isAuthenticated()) {
         req.flash('error', 'You must be signed in');
@@ -10,6 +13,7 @@ module.exports.isLoggedIn = (req, res, next) => {
     next();
 }
 
+//A middleware to check if the user is the auhtor of the recipe
 module.exports.isAuthor = async (req, res, next) => {
     const { id } = req.params;
     const recipe = await Recipe.findById(id);
@@ -20,6 +24,7 @@ module.exports.isAuthor = async (req, res, next) => {
     next();
 }
 
+//A middleware to check if the user is the author of the review
 module.exports.isReviewAuthor = async (req, res, next) => {
     const { id, reviewId } = req.params;
     const review = await Review.findById(reviewId);
