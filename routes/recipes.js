b9549dd481bc73258501419fa3f8b6a837e74f58
EditaGinteryteSidlauskiene <122540484+EditaGinteryteSidlauskiene@users.noreply.github.com>
2023-05-03 10:05:00 +0300

    Update recipes.js
    Comments added

--- routes/recipes.js
+++ routes/recipes.js
@@ -1,3 +1,5 @@
+//This is file consists of routes for recipes
+
 const express = require('express');
 const router = express.Router();
 const recipes = require('../controllers/recipes');
@@ -10,18 +12,22 @@ const upload = multer({ storage });
 router.route('/')
     //Display the list of all recipes
     .get(catchAsync(recipes.index))
+    //Saving created recipe
     .post(isLoggedIn, upload.single('image'), catchAsync(recipes.createRecipe))
 
-//Add a new recipe
+//Displaying a form to create a new recipe
 router.get('/new', isLoggedIn, recipes.renderNewForm);
 
 router.route('/:id')
     //Show the chosen recipe
     .get(isLoggedIn, catchAsync(recipes.showRecipe))
+    //Updating recipe
     .put(isLoggedIn, isAuthor, upload.single('image'), catchAsync(recipes.updateRecipe))
+    //Deleting recipe
     .delete(isLoggedIn, isAuthor, catchAsync(recipes.deleteRecipe))
 
 //Edit the recipe
 router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(recipes.renderEditForm));
 
+//Exporting the router
 module.exports = router;
