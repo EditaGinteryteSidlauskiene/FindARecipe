fc79524a9caef9611f48eb6102b8ad174fd99147
EditaGinteryteSidlauskiene <122540484+EditaGinteryteSidlauskiene@users.noreply.github.com>
2023-05-03 09:41:07 +0300

    Update user.js
    Comments added

--- models/user.js
+++ models/user.js
@@ -1,7 +1,10 @@
+//In this file, the model for user is created
+
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const passportLocalMongoose = require('passport-local-mongoose');
 
+//Creating a schema
 const UserSchema = new Schema({
     email: {
         type: String,
@@ -10,6 +13,10 @@ const UserSchema = new Schema({
     }
 });
 
+//Passing the result of requiring the package 'passport-local-mongoose'.
+//This is going to add on to the schema: username, field for password.
+//It will make sure that usernames are unique
 UserSchema.plugin(passportLocalMongoose);
 
+//Exporting the schema
 module.exports = mongoose.model('User', UserSchema);
