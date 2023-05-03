dd523d14a74e25bf8c75421d7380439ac5d23356
EditaGinteryteSidlauskiene <122540484+EditaGinteryteSidlauskiene@users.noreply.github.com>
2023-05-03 10:19:28 +0300

    Update ExpressError.js
    Comments added

--- utils/ExpressError.js
+++ utils/ExpressError.js
@@ -1,3 +1,6 @@
+//This file is used handle errors
+
+//In case of an error, shwoing message and error status code
 class ExpressError extends Error {
     constructor(message, statusCode) {
         super();
