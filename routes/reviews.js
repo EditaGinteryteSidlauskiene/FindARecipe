062d3d3dbeb3ba880f4173b794d7c4eea989a5aa
EditaGinteryteSidlauskiene <122540484+EditaGinteryteSidlauskiene@users.noreply.github.com>
2023-05-03 10:05:55 +0300

    Update reviews.js
    Comments added

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,3 +1,5 @@
+//This file consists of routes for reviews
+
 const express = require('express');
 const router = express.Router({ mergeParams: true });
 const { isLoggedIn, isReviewAuthor } = require('../middleware');
@@ -5,10 +7,11 @@ const reviews = require('../controllers/reviews');
 
 const catchAsync = require('../utils/catchAsync');
 
-//Save review
+//Save a review
 router.post('/', isLoggedIn, catchAsync(reviews.createReview));
 
 //Delete review
 router.delete('/:reviewId', isLoggedIn, isReviewAuthor, catchAsync(reviews.deleteReview));
 
+//Exporting the router
 module.exports = router;
