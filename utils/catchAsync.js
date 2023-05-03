0caa15f7620b25728c2351b9d0ade53dc613fd46
EditaGinteryteSidlauskiene <122540484+EditaGinteryteSidlauskiene@users.noreply.github.com>
2023-05-03 10:27:36 +0300

    Update catchAsync.js
    Comments added

--- utils/catchAsync.js
+++ utils/catchAsync.js
@@ -1,3 +1,6 @@
+//This file is used to define a catchAsync function, so there would need to
+//add an error handling in every asynch function
+
 module.exports = func => {
     return (req, res, next) => {
         func(req, res, next).catch(next);
